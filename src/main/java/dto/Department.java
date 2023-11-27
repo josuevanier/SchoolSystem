4c8f8fcf5cbcca40d18799c0f6065ec580289f2e
josue <6257586@edu.vaniercollege.qc.ca>
2023-11-26 19:23:53 -0500

    New class department with a method toString

---
+++ src/main/java/dto/Department.java
@@ -0,0 +1,15 @@
+package dto;
+
+public class Department {
+    private String id;
+    private int nextId;
+    private String departmentName;
+
+    /**
+     * String method that return a string.
+     * @return a String
+     */
+    public String toString(){
+        return "";
+    }
+}
