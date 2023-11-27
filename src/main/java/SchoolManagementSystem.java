0cfd5343bc3fd69061b5879c1517622cf875af8f
josue <6257586@edu.vaniercollege.qc.ca>
2023-11-26 19:58:25 -0500

    Method department and documentation

--- src/main/java/SchoolManagementSystem.java
+++ src/main/java/SchoolManagementSystem.java
@@ -1,3 +1,12 @@
 public class SchoolManagementSystem {
 
+    /**
+     * Find department
+     * @param departementId the id of the department
+     * @return  info on the departement
+     */
+    public  String findDepartment(String departementId){
+
+    }
+
 }
