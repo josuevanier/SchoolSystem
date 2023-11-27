4b16e8265998da63e6f76e752fb5b3ba37af83b3
josue <6257586@edu.vaniercollege.qc.ca>
2023-11-26 19:18:06 -0500

    New class department with a method toString

--- src/main/java/dto/Student.java
+++ src/main/java/dto/Student.java
@@ -2,9 +2,10 @@ package dto;
 
 public class Student {
     private String fname;
-    private String lname;
+    private String name;
     private String id;
     private int nextId;
+    private int courseNum;
 
     /**
      * String method that return a string.
