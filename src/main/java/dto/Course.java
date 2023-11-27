35ac2fd6d3224d2c1848f52a56309af7e75f8ca7
josue <6257586@edu.vaniercollege.qc.ca>
2023-11-26 19:33:47 -0500

    New class Teacher with a method toString slightly modified course array.

--- src/main/java/dto/Course.java
+++ src/main/java/dto/Course.java
@@ -3,7 +3,7 @@ package dto;
 public class Course {
     private double credit;
     private String id;
-    private Student students[];
+    private Student[] students;
     private Teacher teacher;
     private  String courseName;
     public String toString(){
---
+++ src/main/java/dto/Teacher.java
@@ -0,0 +1,12 @@
+package dto;
+
+public class Teacher {
+    private String lname;
+    private String fname;
+    private Department department;
+    private String id;
+
+    public String toString(){
+        return "";
+    }
+}
