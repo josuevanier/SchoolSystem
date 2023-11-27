413b3a28bf43451f949702eb58dd16118916082d
josue <6257586@edu.vaniercollege.qc.ca>
2023-11-26 19:27:17 -0500

    New class course with a method toString

---
+++ src/main/java/dto/Course.java
@@ -0,0 +1,12 @@
+package dto;
+
+public class Course {
+    private double credit;
+    private String id;
+    private Student students[];
+    private Teacher teacher;
+    private  String courseName;
+    public String toString(){
+        return "";
+    }
+}
