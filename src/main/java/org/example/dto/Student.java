c96531c15537e47bd59c4126d4bcea14342e010f
josue <6257586@edu.vaniercollege.qc.ca>
2023-12-05 09:12:59 -0500

    Get countstudent from school management

--- src/main/java/org/example/dto/Student.java
+++ src/main/java/org/example/dto/Student.java
@@ -1,31 +1,36 @@
-package dto;
+package org.example.dto;
+
+import lombok.Getter;
 
 /**
  * Contains the info of a student
  * @author Joseph Josue Forestal
  */
+@Getter
 public class Student {
     private String fname;
     private String name;
     private String id;
-    private int nextId = 1;
+
     private int courseNum;
     private Department department;
     private Course[] courses;
+    private SchoolManagementSystem  schoolManagementSystem;
 
     /**
      * Contains the info of a student as an object
      * @param fname fname of student
-     * @param lname last name of student
-     * @param id id of student
+     * @param name last name of student
+     * @param department student
      */
-    public Student(String fname, String lname, Department department){
+    public Student(String fname, String name, Department department, SchoolManagementSystem schoolManagementSystem
+    ){
         this.fname = fname;
         this.name = name;
-        this.id = String.format("COO%d", nextId++)
+        this.id = String.format("COO%d", schoolManagementSystem.getStudentCount());
         this.courseNum = 0;
         this.department = department;
-        this.courses = new Course[MAX_COURSES];
+        this.courses = new Course[SchoolManagementSystem.getMAX_COURSES()];
     }
 
     /**
@@ -33,7 +38,6 @@ public class Student {
      * @return a String
      */
     public String toString(){
-
         return "";
     }
 }
