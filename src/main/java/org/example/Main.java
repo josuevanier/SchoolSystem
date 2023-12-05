5b551e837c3fdb9edf3f88bf41df90f1a8761fb4
josue <6257586@edu.vaniercollege.qc.ca>
2023-12-04 21:31:30 -0500

    Added method can print some of them but still I had an issue with jdk so it could not print properly .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,19 @@
 package org.example;
 
+import org.example.dto.SchoolManagementSystem;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        SchoolManagementSystem c1 = new SchoolManagementSystem();
 
-        printTeacher();
-        modifyCourseTeacher();
-        printDepartment();
-        printStudent();
-        modifyCourseTeacher();
+        c1.addDepartment("JO");
+
+        c1.printDepartment();
+        System.out.println(c1);
+
+
+
     }
 }
