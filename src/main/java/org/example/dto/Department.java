5c5d24d573dfa32858a88e63fad74cc98b5aeeea
josue <6257586@edu.vaniercollege.qc.ca>
2023-12-10 14:51:44 -0500

    Added a toString method.

--- src/main/java/org/example/dto/Department.java
+++ src/main/java/org/example/dto/Department.java
@@ -1,18 +1,18 @@
 package org.example.dto;
 
 import lombok.Getter;
-
+import lombok.ToString;
 
 /**
  * Contains the info of a department
  * @author  Joseph Josue Forestal
  */
 @Getter
+@ToString
 public class Department {
     private String id;
     private String departmentName;
     private  int nextId = 1;
-    private SchoolManagementSystem schoolManagementSystem;
 
     /**
      * @param departmentName department name
@@ -20,14 +20,5 @@ public class Department {
     public Department( String departmentName) {
         this.id = String.format("DOO%d", nextId++);
         this.departmentName = departmentName;
-    }
-
-    /**
-     * String method that return a string.
-     *
-     * @return a String
-     */
-    public String toString() {
-        return String.format("Id is %s, Department name: %s", id, departmentName);
     }
 }
