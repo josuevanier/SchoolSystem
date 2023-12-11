5543d6823d91315a6aad51b8764a66427574d63b
josue <6257586@edu.vaniercollege.qc.ca>
2023-12-10 20:30:01 -0500

    Department as a field to put department name.

--- src/main/java/org/example/dto/Course.java
+++ src/main/java/org/example/dto/Course.java
@@ -1,38 +1,42 @@
 package org.example.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Contains the info a course
  * @author Joseph Josue Forestal
  */
 @Getter
+@Setter
 public class Course {
     private double credit;
     private String id;
     private Student[] students;
     private Teacher teacher;
     private  String courseName;
+    private  Department department;
     private int studentNum;
-    private static int nextId = 1;
+    private  int nextId = 1;
 
     /**
      * Constructor to create the object course
      * @param courseName the courseName of the cours
-     * @param credit
+     * @param credit credit of the course
      */
     public Course(String courseName, double credit, Department department){
-
         this.id = String.format("COO%d", nextId++);
         this.credit = credit;
         this.courseName = courseName;
         this.studentNum = 1;
+        this.department = department;
         this.students = new Student[SchoolManagementSystem.getMAX_STUDENTS()];
     }
     @Override
     public String toString() {
-        return String.format("Course{Id : %s |Credit: %f |course name : %s | Course teacher  : %s}\n",
-                id, credit, courseName, (teacher != null) ? teacher.getName() : "No teacher for this course");
+        return String.format("Course{Id : %s |Credit: %f |course name : %s | Course teacher  : %s | Department : %s}\n",
+                id, credit, courseName, (teacher != null) ? teacher.getName() : "No teacher for this course",
+                department.getDepartmentName());
     }
 }
 
